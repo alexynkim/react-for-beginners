253c0820cd371c02193eb83fd09d7b3151bbee5b
alexynkim <alex.yongnamkim@gmail.com>
2025-03-17 02:29:13 -0700

    Refine Home page layout

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,19 +1,18 @@
 import { Link } from "react-router-dom";
+import style from "./Home.module.css";
 
 function Home() {
   return (
     <div>
-      <h1>Select Project</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/movies">Movie Theater</Link>
-          </li>
-          <li>
-            <Link to="/crypto">Crypto Currenies</Link>
-          </li>
-        </ul>
-      </nav>
+      <h1>My Portfolio</h1>
+      <div className={style.container}>
+        <Link to="/movies" className={style.AppItem}>
+          Movie Theater
+        </Link>
+        <Link to="/crypto" className={style.AppItem}>
+          Crypto Currenies
+        </Link>
+      </div>
     </div>
   );
 }
